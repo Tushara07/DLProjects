4819139f07f7aabf2f3d910290575db2d96ed762
Tushara07 <tusharakatamreddy@.com>
2026-06-15 11:41:43 +0530

    Update CNN app.py

--- CNN/app/app.py
+++ CNN/app/app.py
@@ -18,14 +18,90 @@ MODELS_DIR = os.path.join(
 )
 
 
+from tensorflow.keras.models import Sequential
 
-model = load_model(
-    os.path.join(
-        MODELS_DIR,
-        "fashion_mnist_cnn.keras"
+from tensorflow.keras.layers import (
+    Conv2D,
+    MaxPooling2D,
+    Flatten,
+    Dense,
+    Dropout
+)
+
+
+model = Sequential([
+
+    Conv2D(
+
+        32,
+
+        (3,3),
+
+        activation="relu",
+
+        input_shape=(28,28,1)
+
     ),
-    compile=False,
-    safe_mode=False
+
+    MaxPooling2D(
+
+        (2,2)
+
+    ),
+
+    Conv2D(
+
+        64,
+
+        (3,3),
+
+        activation="relu"
+
+    ),
+
+    MaxPooling2D(
+
+        (2,2)
+
+    ),
+
+    Flatten(),
+
+    Dense(
+
+        128,
+
+        activation="relu"
+
+    ),
+
+    Dropout(
+
+        0.5
+
+    ),
+
+    Dense(
+
+        10,
+
+        activation="softmax"
+
+    )
+
+])
+
+
+model.load_weights(
+
+    os.path.join(
+
+        MODELS_DIR,
+
+        "fashion_mnist.weights.h5"
+
+    )
+
 )
 
 class_names = [
